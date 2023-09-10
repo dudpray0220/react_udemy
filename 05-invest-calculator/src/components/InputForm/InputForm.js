c4540dcf1c9d924182bc8c8392eefd9d7f2d020a
YoungHyun Bae <qodudgus0220@naver.com>
2023-09-11 03:24:25 +0900

    투자계산앱 개발

--- 05-invest-calculator/src/components/InputForm/InputForm.js
+++ 05-invest-calculator/src/components/InputForm/InputForm.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './InputForm.module.css';
 
-const InputForm = () => {
+const InputForm = (props) => {
+  const [formData, setFormData] = useState({
+    'current-savings': '',
+    'yearly-contribution': '',
+    'expected-return': '',
+    duration: '',
+  });
+
+  const changeInputHandler = (event) => {
+    const { id, value } = event.target; // 구조 분해 할당
+    setFormData((prevState) => {
+      return { ...prevState, [id]: value }; // 객체 key에 -이 들어가서 [] 감싸줌.
+    });
+  };
+
+  const clickResetHandler = () => {
+    setFormData({
+      'current-savings': '',
+      'yearly-contribution': '',
+      'expected-return': '',
+      duration: '',
+    });
+  };
+
+  const clickSubmitHandler = (event) => {
+    event.preventDefault();
+    props.onSubmitData(formData);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={clickSubmitHandler}>
       <div className={styles['input-group']}>
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            type="number"
+            id="current-savings"
+            value={formData['current-savings']}
+            onChange={changeInputHandler}
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            id="yearly-contribution"
+            value={formData['yearly-contribution']}
+            onChange={changeInputHandler}
+          />
         </p>
       </div>
       <div className={styles['input-group']}>
@@ -19,15 +57,29 @@ const InputForm = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            id="expected-return"
+            value={formData['expected-return']}
+            onChange={changeInputHandler}
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            type="number"
+            id="duration"
+            value={formData.duration}
+            onChange={changeInputHandler}
+          />
         </p>
       </div>
       <p className={styles.actions}>
-        <button type="reset" className={styles.buttonAlt}>
+        <button
+          type="reset"
+          className={styles.buttonAlt}
+          onClick={clickResetHandler}
+        >
           Reset
         </button>
         <button type="submit" className={styles.button}>
--- 05-invest-calculator/src/components/ResultTable/ResultTable.js
+++ 05-invest-calculator/src/components/ResultTable/ResultTable.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import styles from './ResultTable.module.css';
 
-const ResultTable = () => {
+const ResultTable = (props) => {
+  if (props.result.length === 0) {
+    return <p>No result</p>;
+  }
+
+  let resultData = props.result.map((year) => {
+    return (
+      <tr key={year.year}>
+        <td>{year.year}</td>
+        <td>{year.savingsEndOfYear}</td>
+        <td>{year.yearlyInterest}</td>
+        <td>{year.yearlyContribution}</td>
+        <td>{year.yearlyContribution + year.yearlyInterest}</td>
+      </tr>
+    );
+  });
+
   return (
     <table className={styles.result}>
       <thead>
@@ -13,15 +29,7 @@ const ResultTable = () => {
           <th>Invested Capital</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <td>YEAR NUMBER</td>
-          <td>TOTAL SAVINGS END OF YEAR</td>
-          <td>INTEREST GAINED IN YEAR</td>
-          <td>TOTAL INTEREST GAINED</td>
-          <td>TOTAL INVESTED CAPITAL</td>
-        </tr>
-      </tbody>
+      <tbody>{resultData}</tbody>
     </table>
   );
 };
