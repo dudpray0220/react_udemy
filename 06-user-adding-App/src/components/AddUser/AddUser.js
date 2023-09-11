ec3943b5a463164208c4179cba123b59c5d76e08
YoungHyun Bae <qodudgus0220@naver.com>
2023-09-11 23:10:04 +0900

    사용자 추가 앱 개발 및 리팩토링

--- 06-user-adding-App/src/components/AddUser/AddUser.js
+++ 06-user-adding-App/src/components/AddUser/AddUser.js
@@ -1,25 +1,76 @@
 import React, { useState } from 'react';
 import classes from './AddUser.module.css';
-import Button from '../Button/Button';
-import Card from '../Card/Card';
+import Button from '../UI/Button';
+import Card from '../UI/Card';
+import ErrorModal from '../ErrorModal/ErrorModal';
 
 const AddUser = (props) => {
-  const [userInput, setUserInput] = useState(null);
+  const [userName, setUserName] = useState('');
+  const [userAge, setUserAge] = useState('');
+  const [error, setError] = useState(null);
 
-  const changeHandler = (event) => {};
+  const setNameHandler = (event) => setUserName(event.target.value);
+  const setAgeHandler = (event) => setUserAge(event.target.value);
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    // 유효성 검사 -> 에러 모달 띄움
+    if (userName.trim().length === 0 || userAge.trim().length === 0) {
+      setError({
+        title: 'Invalid Input',
+        message: 'Please enter a valid input value.',
+      });
+      return;
+    }
+    if (+userAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age ( > 0).',
+      });
+      return;
+    }
+    props.onAddUser(userName, userAge);
+
+    setUserName('');
+    setUserAge('');
+    console.log(userName, userAge);
+  };
 
   return (
-    <Card>
-      <form className={classes.input}>
-        <label id={'userName'}>Username</label>
-        <input id={'userName'} type={'text'} onChange={changeHandler} />
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card>
+        <form className={classes.input} onSubmit={submitHandler}>
+          <label htmlFor={'username'}>Username</label>
+          <input
+            id={'username'}
+            type={'text'}
+            value={userName}
+            onChange={setNameHandler}
+          />
 
-        <label id={'userAge'}>Age (Years)</label>
-        <input id={'userAge'} type={'text'} onChange={changeHandler} />
+          <label htmlFor={'age'}>Age (Years)</label>
+          <input
+            id={'age'}
+            type={'number'}
+            value={userAge}
+            onChange={setAgeHandler}
+          />
 
-        <Button>Add User</Button>
-      </form>
-    </Card>
+          <Button type={'submit'}>Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
--- 06-user-adding-App/src/components/UI/Button.js
+++ 06-user-adding-App/src/components/UI/Button.js
@@ -3,7 +3,11 @@ import classes from './Button.module.css';
 
 const Button = (props) => {
   return (
-    <button type={'submit'} className={classes.button}>
+    <button
+      className={classes.button}
+      type={props.type || 'button'}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
